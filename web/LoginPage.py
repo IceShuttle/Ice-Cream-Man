d62d52d346874c5ec3528f96684e46b5b66aceee
MineRanger <MineRanger@debian>
2023-06-11 21:18:08 +0530

    Onetime_login_working

---
+++ web/LoginPage.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import db
+
+st.set_page_config(page_title="Login Page", page_icon="🔒")
+st.title("Login Here")
+
+def login():
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        db.connect_db(username,password)
+        if st.session_state['is_connected']:
+            st.success("Logged in!")
+        else:
+            st.error("Invalid username or password")
+
+    if st.button("LogOut"):
+        db.disconnect()
+
+login()
--- web/db.py
+++ web/db.py
@@ -1,15 +1,44 @@
 from mysql import connector
+import streamlit as st
 
-conn_params = {
-    "user":"C1",
-    "password":"secret",
-    "host":"localhost",
-    "port":"8081",
-    "database":"SHOP"
-}
-connection = connector.connect(**conn_params)
-cursor = connection.cursor()
-# cursor.execute("DESC ITEMS")
-print(cursor.fetchall())
-cursor.close()
-connection.close()
+
+def connect_db(user,secret,host="localhost",port="8081"):
+    if 'is_connected' in st.session_state and st.session_state['is_connected']:
+        st.error("Already Connected Please Log Out")
+        return
+
+    conn_params = {
+        "user":user,
+        "password":secret,
+        "host":host,
+        "port":port,
+        "database":"SHOP"
+    }
+    try: 
+        connection = connector.connect(**conn_params)
+        cursor = connection.cursor()
+        is_connected=True
+        if 'cursor' not in st.session_state:
+            st.session_state['cursor'] = cursor
+        if 'connection' not in st.session_state:
+            st.session_state['connection'] = connection
+        if 'is_connected' not in st.session_state:
+            st.session_state['is_connected'] = is_connected
+
+
+    except connector.Error as err:
+        st.session_state['is_connected'] = False
+        print(f"Error: {err}")
+
+
+def disconnect():
+    if 'is_connected' in st.session_state and st.session_state['is_connected']==False:
+        st.error("Already Logged Out")
+        return
+    try:
+        st.session_state['cursor'].close()
+        st.session_state['connection'].close()
+        st.session_state['is_connected'] = False
+        st.success("Logged Out")
+    except:
+        st.error("Already Logged Out")
